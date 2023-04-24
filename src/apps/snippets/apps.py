24ea11a6b1cd7366bec8f994944d7524e9ffcf81
takeYY <t0k6yist@gmail.com>
2023-04-24 09:27:24 +0900

    modify: files structure

--- src/apps/snippets/apps.py
+++ src/apps/snippets/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 
 class SnippetsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "snippets"
+    name = "apps.snippets"
--- src/manage.py
+++ src/manage.py
@@ -16,7 +16,7 @@ load_dotenv(dotenv_path)
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
